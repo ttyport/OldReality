c986f5a7893fec6c8f0df8d78cb14b41c63aa8e7
yayguy4618 <68721523+yayguy4618@users.noreply.github.com>
2021-01-04 19:45:12 +0900

    Deleted small resolution

--- pong.py
+++ pong.py
@@ -5,8 +5,7 @@ from configmodel import Config
 
 configs = [
     Config('4k', 1600, 1400),
-    Config('fullhd', 800, 700),
-    Config('small', 400, 350)
+    Config('fullhd', 800, 700)
 ]
 
 # Инициализация pygame
