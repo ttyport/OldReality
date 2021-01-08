0e817e0c97cf897ad4d1d13d40e6b190680dc2f7
Kudyakov Artem <t3m8ch@yandex.ru>
2021-01-08 17:58:21 +0400

    Adding a control instruction to pong.

--- pong.py
+++ pong.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import json
 
+from common.learning_controls import KeySurface, LearningControlsSurface
 from configmodel import Config
 
 configs = [
@@ -29,7 +30,7 @@ with open("config.txt", encoding="utf-8") as conf:
 with open(f"resources/langs/pong/{lang}.json", encoding="utf-8") as text:
     data = json.load(text)
 
-k = 1600 / screen_width
+k = 1600 // screen_width
 
 ball_size = 30 / k
 play_height = 1198 / k
@@ -181,7 +182,9 @@ def main():
             pygame.draw.rect(window, green, border, 5)
         else:
             window.fill((0, 0, 0))
-            draw_text_middle(data["paused"], int(60 / k), (0, 255, 0), window)
+            main_menu_surface = get_instruction(data["paused"])
+            main_menu_rect = main_menu_surface.get_rect(center=(screen_width // 2, screen_height // 2))
+            window.blit(main_menu_surface, main_menu_rect)
             pygame.display.update()
             paused = check_pause(paused)
             continue
@@ -206,12 +209,38 @@ def check_pause(paused):
     return paused
 
 
+def get_instruction(title: str, padding=60 // k) -> pygame.Surface:
+    title_font = pygame.font.Font("resources/fonts/font.ttf", 120 // k)
+    key_text_font = pygame.font.Font("resources/fonts/font.ttf", 60 // k)
+
+    key_paddings = (30//k, 20//k)
+    title_indent = 20//k
+
+    arrow_up_text = KeySurface(key_text_font.render("↑", True, (0, 255, 0)), (0, 255, 0), (0, 0, 0), key_paddings)
+    arrow_down_text = KeySurface(key_text_font.render("↓", True, (0, 255, 0)), (0, 255, 0), (0, 0, 0), key_paddings)
+    space_text = KeySurface(key_text_font.render(data["space"].upper(), True, (0, 255, 0)), (0, 255, 0), (0, 0, 0),
+                            key_paddings)
+    escape_text = KeySurface(key_text_font.render("ESC", True, (0, 255, 0)), (0, 255, 0), (0, 0, 0), key_paddings)
+
+    instructions = [
+        {"keys": [arrow_up_text, arrow_down_text], "text": data["instructions"]["move_player"]},
+        {"keys": [space_text], "text": data["instructions"]["pause"]},
+        {"keys": [escape_text], "text": data["instructions"]["quit"]}
+    ]
+
+    main_menu_surface = LearningControlsSurface(title, (0, 255, 0), instructions, padding,
+                                                title_indent, key_text_font, title_font)
+    return main_menu_surface
+
+
 def main_menu():
     run = True
     while run:
         window.fill((0, 0, 0))
 
-        draw_text_middle(data["start_text"], int(120 / k), (0, 255, 0), window)
+        main_menu_surface = get_instruction(data["start_text"])
+        main_menu_rect = main_menu_surface.get_rect(center=(screen_width//2, screen_height//2))
+        window.blit(main_menu_surface, main_menu_rect)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
