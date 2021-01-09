9e3aad9e0ac442a76a90eb5e3204db0959dc3ec8
yayguy4618 <yayguy.4618@yandex.com>
2021-01-09 20:11:53 +0900

    Added help in settings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import pygame
 import json
-from common.learning_controls import LearningControlsSurface, KeySurface
+from common.learning_controls import LearningControlsSurface, KeySurface, InstructionSurface
 from configmodel import Config
 
 
@@ -39,10 +39,10 @@ def draw_text_middle(text, size, color, surface, delta_x=0, delta_y=0, x=None, l
     _x = x if x else screen_width / 2
     if left:
         surface.blit(label, (_x + delta_x,
-                                screen_height / 2 + delta_y))
+                             screen_height / 2 + delta_y))
     else:
         surface.blit(label, (_x - (label.get_width() / 2) + delta_x,
-                                screen_height / 2 - label.get_height() * 2 + delta_y))
+                             screen_height / 2 - label.get_height() * 2 + delta_y))
 
 
 def label_size():
@@ -67,10 +67,52 @@ def draw_list():
                          delta_y=i * 100 / k - 200 / k, left=True)
 
 
+def draw_help():
+    key_text_font = pygame.font.Font("resources/fonts/font.ttf", int(60 // k))
+
+    key_paddings = (30 // k, 20 // k)
+    title_indent = 20 // k
+
+    if not edited:
+        enter_color = (0, 255, 0)
+    else:
+        enter_color = (255, 255, 0)
+
+    arrow_left_text = KeySurface(key_text_font.render("←", True, (0, 255, 0)), (0, 255, 0), (0, 0, 0), key_paddings)
+    arrow_right_text = KeySurface(key_text_font.render("→", True, (0, 255, 0)), (0, 255, 0), (0, 0, 0), key_paddings)
+    arrow_up_text = KeySurface(key_text_font.render("↑", True, (0, 255, 0)), (0, 255, 0), (0, 0, 0), key_paddings)
+    arrow_down_text = KeySurface(key_text_font.render("↓", True, (0, 255, 0)), (0, 255, 0), (0, 0, 0), key_paddings)
+    enter_text = KeySurface(key_text_font.render("ENTER", True, enter_color), enter_color, (0, 0, 0),
+                            key_paddings)
+    escape_text = KeySurface(key_text_font.render("ESC", True, (0, 255, 0)), (0, 255, 0), (0, 0, 0), key_paddings)
+
+    help_surface = pygame.Surface((screen_width, 300 / k))
+    help_surface.blit(arrow_up_text, (5, 50 / k))
+    help_surface.blit(arrow_down_text, (arrow_right_text.get_width() + 5, 50 / k))
+    help_surface.blit(key_text_font.render(data["instructions"]["select"], True, (0, 255, 0)),
+                      (arrow_right_text.get_width() * 2 + 50 / k, 70 / k))
+
+    help_surface.blit(arrow_left_text, (5, 150 / k))
+    help_surface.blit(arrow_right_text, (arrow_right_text.get_width() + 5, 150 / k))
+    help_surface.blit(key_text_font.render(data["instructions"]["edit"], True, (0, 255, 0)),
+                      (arrow_right_text.get_width() * 2 + 50 / k, 180 / k))
+
+    help_surface.blit(enter_text, (arrow_right_text.get_width() * 2 + 550 / k, 50 / k))
+    help_surface.blit(key_text_font.render(data["instructions"]["apply"], True, (0, 255, 0)),
+                      (arrow_right_text.get_width() * 2 + enter_text.get_width() + 600 / k, 70 / k))
+
+    help_surface.blit(escape_text, (arrow_right_text.get_width() * 2 + 550 / k, 150 / k))
+    help_surface.blit(key_text_font.render(data["instructions"]["quit"], True, (0, 255, 0)),
+                      (arrow_right_text.get_width() * 2 + escape_text.get_width() + 600 / k, 180 / k))
+
+    window.blit(help_surface, (100 / k, screen_height - 300 / k))
+
+    border = pygame.Rect(0, screen_height - 300 / k, screen_width, 300 / k)
+    pygame.draw.rect(window, (0, 255, 0), border, 5, border_radius=5)
 
 
 def main_menu():
-    global x_coord
+    global x_coord, edited
     running = True
     while running:
         window.fill((0, 0, 0))
@@ -96,12 +138,13 @@ def main_menu():
                     coord = settings[x_coord].index(config[x_coord]) - 1
                     coord = coord if coord >= 0 else 1
                     config[x_coord] = settings[x_coord][coord]
+                    edited = True
 
                 elif event.key == pygame.K_RIGHT:
                     coord = settings[x_coord].index(config[x_coord]) + 1
                     coord = coord if coord <= 1 else 0
-                    write_config()
                     config[x_coord] = settings[x_coord][coord]
+                    edited = True
 
                 elif event.key == pygame.K_RETURN:
                     write_config()
@@ -112,6 +155,7 @@ def main_menu():
                     main.main()
         update_cursor()
         draw_list()
+        draw_help()
         pygame.display.update()
     pygame.quit()
 
@@ -126,15 +170,15 @@ screen_height = 1400
 
 reconfig()
 
-k = 1600 / screen_width
+edited = False
 
+k = 1600 / screen_width
 
 settings_list = ["Language", "Resolution"]
 config = [lang, res]
 language = ["en", "ru"]
 resolution = ["4K", "FullHD"]
 settings = [language, resolution]
-
 
 x_coord = 0
 
